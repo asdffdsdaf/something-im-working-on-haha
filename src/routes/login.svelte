e4e7a6654668c4c2c9fd6a080a9814d100a42099
asdf <asdffdsdaf@gmail.com>
2022-01-24 17:45:20 +0000

    junk

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -1,1 +1,24 @@
-//TODO: do it
+
+<script>
+    import {Button, TextInput} from "carbon-components-svelte";
+</script>
+<style lang="scss">
+    @import "@carbon/layout";
+    tr {
+        margin-top: $spacing-08;
+    }
+</style>
+<div>
+    <Button>123</Button>
+    <tr style="margin-bottom: $spacing-08;">
+        <h2 class="bx--type-productive-heading-04">
+            <span>Log in</span>
+        </h2>
+        <p>
+            <span>Don't have an account?</span>
+            <a href="/register">Create an account</a>
+        </p>
+        
+    </tr>
+    <TextInput labelText="User name" placeholder="Enter user name..." />
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,14 @@
 import { optimizeImports } from "carbon-preprocess-svelte";
+import { sveltePreprocess } from "svelte-preprocess/dist/autoProcess.js";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: [optimizeImports()],
+  preprocess: [optimizeImports(), sveltePreprocess()],
   kit: {
     target: "#svelte",
+  },
+  css: css => {
+    css.write('public/bundle.css')
   },
 };
 

7dd51659a2fe4e2f5a296e8a650a506663897c41
asdffdsdaf <asdffdsdaf@gmail.com>
2022-02-02 17:08:28 +0300

    Preparing for small rewrite

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -7,22 +7,27 @@
   @import "src/scss/spacing.scss";
 </style>
 
-<div class="s-login-top">
-    <h2 class="bx--type-productive-heading-04">
-        <span>Log in</span>
-    </h2>
-    <p>
-        <span>Don't have an account?</span>
-        <a href="/register">Create an account</a>
-    </p>
-</div>
-
-<div class="s-login-text-field">
-    <TextInput labelText="User name" placeholder="Enter user name..."/>
-</div>
-<div class="s-login-continue-button">
-    <Button icon={ArrowRight16}>Continue</Button>
-</div>
-<div>
-    <Checkbox labelText="Remember Account"/>
+<div class="bx--grid bx--grid--full-width">
+    <div class="s-auth-panel bx--col-md-4 bx--col-lg-4">
+        <div class="s-login-top">
+            <h2 class="bx--type-productive-heading-04">
+                <span>Log in</span>
+            </h2>
+            <p>
+                <span>Don't have an account?</span>
+                <a href="/register">Create an account</a>
+            </p>
+        </div>
+        <tr>
+        <div class="s-login-text-field">
+            <TextInput labelText="User name" placeholder="Enter user name..."/>
+        </div>
+        <div class="s-login-continue-button">
+            <Button icon={ArrowRight16}>Continue</Button>
+        </div>
+        </tr>
+        <div>
+            <Checkbox labelText="Remember Account"/>
+        </div>
+    </div>
 </div>
